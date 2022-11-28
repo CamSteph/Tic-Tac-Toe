45ac3d38791ef674bce600546ce98e7d66c595d2
CamSteph <camstephenson2017@gmail.com>
2022-11-28 00:27:55 -0500

    Updating bot move logic

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -228,11 +228,14 @@ const GameBoard = ({botName}) => {
     else if ( arrOfTiles[1] === markToCheck && arrOfTiles[1] === arrOfTiles[4] && !arrOfTiles[7] ) {
       return 7;
     }
-    else if ( arrOfTiles[4] === markToCheck && arrOfTiles[4] === arrOfTiles[7] && !arrOfTiles[0] ) {
-      return 0;
+    else if ( arrOfTiles[4] === markToCheck && arrOfTiles[4] === arrOfTiles[7] && !arrOfTiles[1] ) {
+      return 1;
     }
     else if ( arrOfTiles[2] === markToCheck && arrOfTiles[2] === arrOfTiles[5] && !arrOfTiles[8] ) {
       return 8;
+    }
+    else if ( arrOfTiles[5] === markToCheck && arrOfTiles[5] === arrOfTiles[8] && !arrOfTiles[2] ) {
+      return 2;
     }
     else if ( arrOfTiles[4] === markToCheck && arrOfTiles[4] === arrOfTiles[8] && !arrOfTiles[0] ) {
       return 0;
@@ -245,6 +248,9 @@ const GameBoard = ({botName}) => {
     }
     else if ( arrOfTiles[2] === markToCheck && arrOfTiles[2] === arrOfTiles[4] && !arrOfTiles[6] ) {
       return 6;
+    }
+    else if ( arrOfTiles[4] === markToCheck && arrOfTiles[4] === arrOfTiles[6] && !arrOfTiles[2] ) {
+      return 2;
     }
     else if ( arrOfTiles[2] === markToCheck && arrOfTiles[2] === arrOfTiles[6] && !arrOfTiles[4] ) {
       return 4;
@@ -259,6 +265,15 @@ const GameBoard = ({botName}) => {
       return 4;
     }
     else if ( arrOfTiles[2] === markToCheck && arrOfTiles[2] === arrOfTiles[8] && !arrOfTiles[5] ) {
+      return 4;
+    }
+    else if ( arrOfTiles[0] === markToCheck && arrOfTiles[0] === arrOfTiles[2] && !arrOfTiles[1] ) {
+      return 1;
+    }
+    else if ( arrOfTiles[6] === markToCheck && arrOfTiles[6] === arrOfTiles[8] && !arrOfTiles[7] ) {
+      return 7;
+    }
+    else if ( arrOfTiles[3] === markToCheck && arrOfTiles[3] === arrOfTiles[5] && !arrOfTiles[4] ) {
       return 4;
     }
   };
