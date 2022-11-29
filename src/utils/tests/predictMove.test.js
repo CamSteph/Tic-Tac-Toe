5bc47e68ec75b04ddbee4cd2fcc823b4bb8f8c47
CamSteph <camstephenson2017@gmail.com>
2022-11-28 19:49:38 -0500

    Add tests for bot defensive and offensive moves

--- src/utils/tests/predictMove.test.js
+++ src/utils/tests/predictMove.test.js
@@ -4,40 +4,46 @@ import { predictMove } from '../predictMove';
 describe('Board tiles are filled', () => {
 
   
-  it('Top row\'s last tile to be filled', () => {
+  it('Defend top row\'s last tile', () => {
     const arrOfTiles = ['X', 'X', '', '', '', '', '', '', ''];
     const spotToMoveTo = predictMove('X', arrOfTiles);
     expect(spotToMoveTo).toEqual(2);
   });
   
-  it('Bottom row\'s first tile to be filled', () => {
+  it('Defend bottom row\'s first tile', () => {
     const arrOfTiles = ['', '', '', '', '', '', '', 'X', 'X'];
     const spotToMoveTo = predictMove('X', arrOfTiles);
     expect(spotToMoveTo).toEqual(6);
   });
   
-  it('Middle column\'s middle tile to be filled', () => {
+  it('Defend middle column\'s middle tile', () => {
     const arrOfTiles = ['', 'X', '', '', '', '', '', 'X', ''];
     const spotToMoveTo = predictMove('X', arrOfTiles);
     expect(spotToMoveTo).toEqual(4);
   });
   
-  it('Last columns\'s middle tile to be filled', () => {
+  it('Defend last columns\'s middle tile', () => {
     const arrOfTiles = ['', '', 'X', '', '', '', '', '', 'X'];
     const spotToMoveTo = predictMove('X', arrOfTiles);
     expect(spotToMoveTo).toEqual(5);
   });
   
-  it('Left to right diagnal middle tile to be filled', () => {
+  it('Defend left to right diagnal middle tile', () => {
     const arrOfTiles = ['X', '', '', '', 'X', '', '', '', ''];
     const spotToMoveTo = predictMove('X', arrOfTiles);
     expect(spotToMoveTo).toEqual(8);
   });
   
-  it('Right to left diagnal middle tile to be filled', () => {
+  it('Defend right to left diagnal middle tile', () => {
     const arrOfTiles = ['', '', 'X', '', 'X', '', '', '', ''];
     const spotToMoveTo = predictMove('X', arrOfTiles);
     expect(spotToMoveTo).toEqual(6);
+  });
+  
+  it('Move to middle tile on last row', () => {
+    const arrOfTiles = ['', '', '', '', '', '', 'O', '', 'O'];
+    const spotToMoveTo = predictMove('O', arrOfTiles);
+    expect(spotToMoveTo).toEqual(7);
   });
 
 });
